a7e11bb797808a56081eaa754a21baeb3f3df794
Kusum Sharma <Kusum@Kuss-MBP.fios-router.home>
2022-11-29 20:08:02 -0500

    Initial plan

---
+++ 2048.py
@@ -0,0 +1,23 @@
+# Game 2048
+# 2048 is a board game with nxn dimensions which fills with the number 2
+# and 4. The goal of the user is to add identical numbers til 2048.
+
+
+def main():
+# Design: 
+    # Initialize the board
+    #   - start with a board with two 2s randomly placed.
+
+    # Moves
+    #   - up, down, right and left
+    #   - all elements in the board will move to the furthest direction
+
+    # After every move 
+    #   - fill random cell with a 2 or a 4
+
+    # WIN
+    # - all elements must add upto 2048
+
+    # GAME OVER
+    # - no empty spaces left to fill with a 2 or a 4
+main()
