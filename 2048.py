63aacbff662509f385c9d12b0e12d64dbe58c3ee
Kusum Sharma <skusum70@gmail.com>
2022-12-11 21:02:54 -0500

    Update the  Move functions

--- 2048.py
+++ 2048.py
@@ -10,52 +10,56 @@ PM = [] #Stores row and col of present element
 # Design: 
     # Initialize the board
     #   - start with a board with two 2s randomly placed.
-def printBoard(board):
+def createBoard(board):
     for i in range(LEN):
         col = []
         for j in range(LEN):
-            col.append("_")
+            col.append(0)
         board.append(col)
-        print("\t\t",' '.join(board[i]),end = "\n")
-        print()
+
+def printBoard(board):
+    for i in range(LEN):
+        for j in range(len(board[i])):
+            #print("\t\t",' '.join(board[i]),end = "\n")
+            print("\t", board[i][j], end=" ")
+        print("\n")
 
     # Get the random row and col to populate the board
-def randMove():
+def randMove(board):
+    currIndex(board)
     randRow = random.randint(0,3)
     randCol = random.randint(0,3)
     # Check for douplicates in random moves and present moves
-    while ([randRow, randCol] in RM or [randRow, randCol] in PM):
+    while ([randRow, randCol] in PM or [randRow, randCol] in RM):
         print("Redo b/c random douplicate found: ", randRow, randCol)
         randRow = random.randint(0,3)
         randCol = random.randint(0,3)
-        return randRow, randCol
     print("The random row and col:", randRow, randCol)
+    print ("PM: " , PM)
     PM.clear()
     return [randRow, randCol]
 
 # After every move and start of the game
 #   - fill random cell with a 2 or a 4
 def populate(board, start):
-    isEmpty(board)
-    print ("PM: " , PM)
     #If its the start of the game populate two 2s
     if (start):
-        RM.append(randMove())
-        board[RM[0][0]][RM[0][1]] = '2'
+        RM.append(randMove(board))
+        board[RM[0][0]][RM[0][1]] = 2
 
     #Insert new moves in the begining of the list for easy indexing
-    RM.insert(0,randMove())
-    board[RM[0][0]][RM[0][1]] = '2'
+    RM.insert(0,randMove(board))
+    board[RM[0][0]][RM[0][1]] = 2
 
-    printBoard(board)
     print (RM)
     RM.clear()
+    printBoard(board)
 
 # Append non-empty cells to present elements
-def isEmpty(board):
+def currIndex(board):
     for i in range(LEN):
         for j in range(LEN):
-            if board[i][j] != '_':
+            if board[i][j] != 0:
                 PM.append([i,j])
 
 def userMove(board):
@@ -78,6 +82,14 @@ def userMove(board):
         moveRight(board)
 
     return userInput
+
+""" def merge(board):
+    for i in range(LEN): 
+        for j in range(LEN-1): 
+            if board[i][j] == board[i][j+1] and board[i][j] != 0: 
+                board[i][j] *= 2 
+                board[i][j+1] = 0
+     """
     # Moves
     #   - up, down, right and left
     #   - all elements in the board will move to the furthest direction
@@ -91,21 +103,13 @@ def moveUp(board):
             if board[i][j] != '_' and board[i-1][j] != '_':
                 #if curr and prev are equal the far left contains the sum
                 if board[i][j] == board[i-1][j]:
-                    board[3][j] = str(int(board[i][j])+ int(board[i-1][j]))
-                    ##print("Board[i][0]", 3, j," is updated to: ", board[3][j])
-                    board[i][j] = '_'
-                    ##print(i,j ,"is empty now")
-                    #make sure prev isn't 3rd row;
-                    if(i-1 != 3):
-                        board[i+1][j] = '_'
-                        ##print(i+1,j, "Next is empty ")
+                    board[i-1][j] = str(int(board[i][j])+ int(board[i-1][j]))
+                    board[i][j] = board[i+1][j]
     
             #if the current isn't empty and not equal to next and next is empty
             elif board[i][j] != '_' and board[i][j] != board[i-1][j] and board[i-1][j] == '_':
                 board[i-1][j] = board[i][j]
                 board[i][j] = '_'
-                ##print("Board[i+1][j]", i-1,j, " is updated to: ", board[i-1][j])
-                #printBoard(board)
     populate(board,False)
 
 def moveDown(board):
@@ -116,18 +120,17 @@ def moveDown(board):
         for j in range(LEN):
             #get board[i] and scan for anything that isnt _
             #if curr isn't empty and next isn't empty
-            if board[i][j] != '_' and board[i+1][j] != '_':
+            if board[i][j] != 0 and board[i][j] == board[i+1][j]:
                 #if curr and next are equal the far down contains the sum
-                if board[i][j] == board[i+1][j]:
-                    board[i+1][j] = str(int(board[i][j])+ int(board[i+1][j]))
-                    board[i][j] = board[i-1][j]
-                    break
+                board[i][j] = board[i][j] + board[i+1][j]
+                board[i+1][j] = board[i][j]
+                board[i][j] = 0
 
             #if the current isn't empty and not equal to next and next is empty
-            if board[i][j] != '_' and board[i][j] != board[i+1][j] and board[i+1][j] == '_':
+            elif board[i][j] != 0 and board[i][j] != board[i+1][j] and board[i+1][j] == 0:
                 board[i+1][j] = board[i][j]
-                board[i][j] = '_'
-                
+                board[i][j] = 0
+            
     populate(board,False)
 
 def moveLeft(board):
@@ -137,24 +140,19 @@ def moveLeft(board):
     for i in range(LEN):
         for j in range(LEN-1,0,-1):
 
-            #get board[i] and scan for anything that isnt _
+            #get board[i] and scan for anything that isnt 0
             #if curr isn't empty and prev isn't empty
-            if board[i][j] != '_' and board[i][j-1] != '_':
-                #if curr and prev are equal the far left contains the sum
-                if board[i][j] == board[i][j-1]:
-                    board[i][0] = str(int(board[i][j])+ int(board[i][j-1]))
-                    ##print("Board[i][0]", i, 0," is updated to: ", board[i][0])
-                    board[i][j] = '_'
-                    ##print(i,j ,"is empty now")
-                    #make sure prev isn't far left; o/w far left would be _ instead of sum
-                    if(j-1 != 0):
-                        board[i][j-1] = '_'
-    
-            #if the current isn't empty and not equal to prev and prev is empty
-            elif board[i][j] != '_' and board[i][j] != board[i][j-1] and board[i][j-1] == '_':
+            if board[i][j] != 0 and board[i][j-1] == board[i][j]:
+                    #Add current starting (0,3) and Left of index (0,2)
+                    board[i][j] = board[i][j]+ board[i][j-1] #current has sum
+                    board[i][j-1] = board[i][j] #Left has sum
+                    board[i][j] = 0 #Make far right 0       
+
+            #Move all the elements to the left
+            #if the current isn't empty and not equal to left and left is empty
+            elif board[i][j] != 0 and board[i][j] != board[i][j-1] and board[i][j-1] == 0:
                 board[i][j-1] = board[i][j]
-                board[i][j] = '_'
-                ##print("Board[i][j-1]", i,j-1, " is updated to: ", board[i][j-1])
+                board[i][j] = 0
             
     populate(board,False)
 
@@ -164,18 +162,20 @@ def moveRight(board):
     for i in range(LEN):
         #Goal : [_ 2 _ _] to move 2 to the right
         for j in range(LEN-1):
-            # No need to care ab moves; get board[i] and scan for anything that isnt _
-            if board[i][j] != '_' and board[i][j+1] != '_':
-                if board[i][j] == board[i][j+1]:
-                    board[i][j+1] = str(int(board[i][j])+ int(board[i][j+1]))
-                    board[i][j] = board[i][j-1]
-                    break
-                       
+            #if two elements are equal add next (0,1) and current (0,0)
+            if board[i][j] != 0 and board[i][j+1] == board[i][j]:
+                board[i][j+1] = board[i][j] + board[i][j+1]
+                #board[i][j] = 0
+                #break
+                board[i][j] = board[i][j+1]
+                board[i][j] = 0
+                break
+            #Move all the elements to the right
             #if the current isn't empty and not equal to next and next is empty
-            if board[i][j] != '_' and board[i][j] != board[i][j+1] and board[i][j+1] == '_':
-                board[i][j+1] = board[i][j]
-                board[i][j] = '_'
-            
+            elif board[i][j] != 0 and board[i][j] != board[i][j+1] and board[i][j+1] == 0:
+                board[i][j+1] = board[i][j] #moves element right
+                board[i][j] = 0 #empties prev
+                
     populate(board,False)
     # After every move 
     #   - fill random cell with a 2 or a 4
@@ -188,11 +188,10 @@ def moveRight(board):
 def gameOver(board):
     for i in range(LEN):
         for j in range(LEN):
-            if '_' in board[i][j]:
+            if board[i][j] == 0:
                 return False
 
     return True
-
 
 
 def main():
@@ -200,15 +199,13 @@ def main():
     start = True #Is it the start of the game? 
     print("\t     Welcome to 2048\n",
             "Begin by joining the numbers and get to 2048.")
-    printBoard(board)
+    createBoard(board)
     populate(board, start)
-    start = False 
+    start = False
     userInput = userMove(board)
     while(userInput != 'q' and userInput != 'Q' and gameOver(board) != True):
         userInput = userMove(board)
         #check gameover or win
         gameOver(board)
     
-
-
 main()
