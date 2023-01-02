82886d1dd8d80f915de4844e1752f3c21b3f9a6d
Kusum Sharma <skusum70@gmail.com>
2023-01-01 23:33:30 -0500

    Fixed all direction and win()

--- 2048.py
+++ 2048.py
@@ -20,22 +20,21 @@ def createBoard(board):
 def printBoard(board):
     for i in range(LEN):
         for j in range(len(board[i])):
-            #print("\t\t",' '.join(board[i]),end = "\n")
             print("\t", board[i][j], end=" ")
         print("\n")
 
-    # Get the random row and col to populate the board
+# Get the random row and col to populate the board
 def randMove(board):
     currIndex(board)
     randRow = random.randint(0,3)
     randCol = random.randint(0,3)
     # Check for douplicates in random moves and present moves
     while ([randRow, randCol] in PM or [randRow, randCol] in RM):
-        print("Redo b/c random douplicate found: ", randRow, randCol)
+        #print("Redo b/c random douplicate found: ", randRow, randCol)
         randRow = random.randint(0,3)
         randCol = random.randint(0,3)
-    print("The random row and col:", randRow, randCol)
-    print ("PM: " , PM)
+    #print("The random row and col:", randRow, randCol)
+    #print ("PM: " , PM)
     PM.clear()
     return [randRow, randCol]
 
@@ -51,7 +50,7 @@ def populate(board, start):
     RM.insert(0,randMove(board))
     board[RM[0][0]][RM[0][1]] = 2
 
-    print (RM)
+    #print (RM)
     RM.clear()
     printBoard(board)
 
@@ -83,70 +82,48 @@ def userMove(board):
 
     return userInput
 
-""" def merge(board):
-    for i in range(LEN): 
-        for j in range(LEN-1): 
-            if board[i][j] == board[i][j+1] and board[i][j] != 0: 
-                board[i][j] *= 2 
-                board[i][j+1] = 0
-     """
-    
     
     # Moves
     #   - up, down, right and left
     #   - all elements in the board will move to the furthest direction
 # Moves elements all the upwards while adding similar ones
+
 def moveUp(board):
-    added = [] #To keep track of whether the elements were already added to avoid adding twice
-    
-    for i in range(LEN-1,0,-1):        
-        for j in range(LEN):
-
-            #Move all the elements to the Up until next remains empty
-            if board[i][j] != 0 and board[i-1][j] == 0:
-                board[i-1][j] = board[i][j] #moves element Up
-                board[i][j] = 0
-
-            #Current isn't empty; current == next and both are not in recently added
-            elif board[i][j] != 0 and board[i][j] == board[i-1][j] and (i, j) not in added and (i-1,j) not in added:
-                #Double the value of the element and empty current
-                board[i-1][j] *= 2
-                board[i][j] = 0
-    
-                added.append((i, j))
-                added.append((i-1, j))
+    for j in range(LEN):
+        added = []  # To keep track of whether the elements were already added to avoid adding twice
+        # Iterate through each element in the current column
+        for i in range(LEN):
+            # Move all the elements up
+            while i - 1 >= 0 and board[i - 1][j] == 0:
+                board[i - 1][j] = board[i][j]
+                board[i][j] = 0  # Empty current
+                i -= 1
+            # Current isn't empty and current == next and both are not in recently added
+            if i - 1 >= 0 and board[i][j] != 0 and board[i][j] == board[i - 1][j] and i not in added and i - 1 not in added:
+                # Double the value of the current element
+                board[i - 1][j] *= 2
+                board[i][j] = 0  # Empty current
+                added.append(i - 1)
 
     populate(board,False)
 
 # Moves elements to the furthest down while adding similar elements once
 def moveDown(board):
-    
-    added = [] 
-    
-    for i in range(LEN-1):
-        for j in range(LEN):
-
-            #Move all the elements downward
-            if board[i][j] != 0 and board[i+1][j] == 0:
-                board[i+1][j] = board[i][j] #moves element right
-                board[i][j] = 0 #empties prev
-
-            #Current isn't empty; current == next and both are not in recently added
-            elif board[i][j] != 0 and board[i][j] == board[i+1][j] and (i, j) not in added and (i+1,j) not in added:
-                # Double the value of the element
-                board[i][j] *= 2
-                board[i+1][j] = board[i][j]
-                board[i][j] = 0
-
-                #WORKEDDD to skip a 0 in the middle
-                if i != 0 and board[i-1][j]!=0:
-                    board[i][j] = board[i-1][j]
-                    board[i-1][j] = 0
-
-                added.append((i, j))
-                added.append((i+1, j))
-            
-
+    for j in range(len(board[0])):
+        added = []  # To keep track of whether the elements were already added to avoid adding twice
+        # Iterate through each element in the current column in reverse order
+        for i in range(LEN - 1, -1, -1):
+            # Move all the elements down
+            while i + 1 < LEN and board[i + 1][j] == 0:
+                board[i + 1][j] = board[i][j]
+                board[i][j] = 0  # Empty current
+                i += 1
+            # Current isn't empty and current == next and both are not in recently added
+            if i + 1 < len(board) and board[i][j] != 0 and board[i][j] == board[i + 1][j] and i not in added and i + 1 not in added:
+                # Double the value of the current element
+                board[i + 1][j] *= 2
+                board[i][j] = 0  # Empty current
+                added.append(i + 1)
 
     populate(board,False)
 
@@ -154,62 +131,42 @@ def moveDown(board):
 
 # Moves elements to the furthest left while adding similar elements once
 def moveLeft(board):
-    for i in range(LEN):
-        
-        added = [] #To keep track of whether the elements were already added to avoid adding twice
-        
-        for j in range(LEN-1,0,-1):
-            
-            #Move all the elements to the left
-            if board[i][j] != 0 and board[i][j-1] == 0:
-                board[i][j-1] = board[i][j] #moves element left
-                board[i][j] = 0
-            
-            #Current isn't empty; current == prev and both are not in recently added
-            elif board[i][j] != 0 and board[i][j] == board[i][j-1] and (i, j) not in added and (i,j-1) not in added:
-                #Double the value of the element and empty current
-                board[i][j] *= 2
-                board[i][j-1] = board[i][j]
-                board[i][j] = 0
-                #WORKEDDD to skip a 0 in the middle
-                if j != 3 and board[i][j+1]!=0:
-                    board[i][j] = board[i][j+1]
-                    board[i][j+1] = 0
+    for row in board:
+        added = []  # To keep track of whether the elements were already added to avoid adding twice
+        # Iterate through each element in the current row
+        for i in range(LEN):
+            # Move all the elements to the left
+            while i - 1 >= 0 and row[i - 1] == 0:
+                row[i - 1] = row[i]
+                row[i] = 0  # Empty current
+                i -= 1
+            # Current isn't empty and current == next and both are not in recently added
+            if i - 1 >= 0 and row[i] != 0 and row[i] == row[i - 1] and i not in added and i - 1 not in added:
+                # Double the value of the current element
+                row[i - 1] *= 2
+                row[i] = 0  # Empty current
+                added.append(i - 1)
 
-                added.append((i, j))
-                added.append((i, j-1))
-    
     populate(board,False)
 
 
 # Moves elements to the furthest right while adding similar elements once
 def moveRight(board):
-    for i in range(LEN):
-        
-        added = [] #To keep track of whether the elements were already added to avoid adding twice
-        
-        for j in range(LEN-1):
-
-            #Move all the elements to the right
-            while board[i][j] != 0 and board[i][j + 1] == 0:
-                board[i][j + 1] = board[i][j] #moves element right
-                board[i][j] = 0 #empties prev
-
-            #Current isn't empty; current == next and both are not in recently added
-            if board[i][j] != 0 and board[i][j] == board[i][j + 1] and (i, j) not in added and (i,j+1) not in added:
-                # Double the value of the element
-                board[i][j] *= 2
-                board[i][j+1] = board[i][j]
-                board[i][j] = 0 #empty current
-                #WORKEDDD to skip a 0 in the middle
-                if j != 0 and board[i][j-1]!=0:
-                    board[i][j] = board[i][j-1]
-                    board[i][j-1] = 0
-
-
-                added.append((i, j))
-                added.append((i, j+1))
-
+    for row in board:
+        added = []  # To keep track of whether the elements were already added to avoid adding twice
+        # Iterate through each element in the current row in reverse order
+        for i in range(LEN - 1, -1, -1):
+            # Move all the elements to the right
+            while i + 1 < LEN and row[i + 1] == 0:
+                row[i + 1] = row[i]
+                row[i] = 0  # Empty current
+                i += 1
+            # Current isn't empty and current == next and both are not in recently added
+            if i + 1 < LEN and row[i] != 0 and row[i] == row[i + 1] and i not in added and i + 1 not in added:
+                # Double the value of the current element
+                row[i + 1] *= 2
+                row[i] = 0  # Empty current
+                added.append(i + 1)
     populate(board,False)
 
 
@@ -218,6 +175,13 @@ def moveRight(board):
 
     # WIN
     # - all elements must add upto 2048
+def win(board):
+    for i in range(LEN):
+        for j in range(LEN):
+            if board[i][j] == 2048:
+                print("*****************************\n\tYOU WIN!!\n*****************************")
+                return True
+    return False
 
 # GAME OVER
 # - no empty spaces left to fill with a 2 or a 4
@@ -226,22 +190,24 @@ def gameOver(board):
         for j in range(LEN):
             if board[i][j] == 0:
                 return False
-
+    print("GAME OVER!")
     return True
 
 
 def main():
     board = []
     start = True #Is it the start of the game? 
-    print("\t     Welcome to 2048\n",
-            "Begin by joining the numbers and get to 2048.")
+    print("\n\n\t     Welcome to 2048\n",
+           "Begin by joining the numbers and get to 2048.\n")
     createBoard(board)
     populate(board, start)
     start = False
     userInput = userMove(board)
-    while(userInput != 'q' and userInput != 'Q' and gameOver(board) != True):
+    while(userInput != 'q' and userInput != 'Q' and gameOver(board) != True and win(board)!= True):
+        win(board)
         userInput = userMove(board)
         #check gameover or win
         gameOver(board)
+        
     
 main()
